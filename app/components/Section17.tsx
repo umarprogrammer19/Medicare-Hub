2573ae5a4fb5840bdc994a70bce664e63bae70ae
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-12-26 14:22:10 +0500

    files

--- app/components/Section17.tsx
+++ app/components/Section17.tsx
@@ -14,45 +14,43 @@ export default function StatsSection() {
         />
       </Head>
 
-      <section className="font-[Poppins] bg-[#050b1f] text-white py-24">
-        <div className="max-w-7xl mx-auto px-6 grid md:grid-cols-2 gap-16 items-center">
+      <section className="font-[Poppins] bg-[#050b1f] text-white py-13">
+        <div className="max-w-7xl mx-auto px-6 grid md:grid-cols-2 gap-14 items-center">
 
           {/* LEFT IMAGE */}
           <div>
             <Image
-              src="/dr.png"   // apni image public folder mein rakho
+              src="/dr.png"
               alt="Doctor"
-              width={500}
-              height={600}
-              className="rounded-lg"
-            />
+              width={420}   
+              height={900} />
           </div>
 
           {/* RIGHT CONTENT */}
-          <div className="grid sm:grid-cols-2 gap-14">
+          <div className="grid sm:grid-cols-2 gap-12">
 
             <StatBox
               number="12"
               title="YEARS WITH YOU"
-              text="Phasellus laoreet tristique sagittis. Phasellus commodo dolor sed elit iaculis interdum."
+              text="Phasellus laoreet tristique sagittis. Phasellus commodo dolor sed elit iaculis interdum"
             />
 
             <StatBox
               number="41"
               title="AWARDS"
-              text="Etiam ante ante, molestie vitae cursus ac, pharetra euismod libero."
+              text="Etiam ante ante, molestie vitae cursus ac, pharetra euismod libero"
             />
 
             <StatBox
               number="25"
               title="DOCTORS"
-              text="Suspendisse sed lorem elit. Aenean nec faucibus lacus. Phasellus ultrices risus vitae eros suscipit."
+              text="Suspendisse sed lorem elit. Aenean nec faucibus lacus. Phasellus ultrices risus vitae eros suscipit"
             />
 
             <StatBox
               number="800"
               title="SATISFIED CLIENTS"
-              text="Mauris vitae varius tortor. Fusce eros diam, auctor sit amet lorem ut, efficitur iaculis magna."
+              text="Mauris vitae varius tortor. Fusce eros diam, auctor sit amet lorem ut, efficitur iaculis magna"
             />
 
           </div>
@@ -66,14 +64,14 @@ export default function StatsSection() {
 function StatBox({ number, title, text }) {
   return (
     <div>
-      <h3 className="text-5xl font-bold mb-3">{number}</h3>
+      <h3 className="text-5xl font-semibold mb-2">{number}</h3>
 
-      <h4 className="text-xl font-semibold tracking-wide mb-3 relative inline-block">
+      <h4 className="text-xl font-semibold tracking-wide mb-2 relative inline-block">
         {title}
         <span className="block w-14 h-[2px] bg-white mt-2"></span>
       </h4>
 
-      <p className="text-gray-300 leading-relaxed">{text}</p>
+      <p className="leading-relaxed">{text}</p>
     </div>
   );
 }
