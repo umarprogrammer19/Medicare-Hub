014dc443b2b6aa334dcf967973e9707f3d3024a6
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-12-28 15:14:44 +0500

    uhh

--- app/components/Section18.tsx
+++ app/components/Section18.tsx
@@ -18,7 +18,6 @@ export default function ContactSection() {
           <h2 className="text-4xl font-bold mb-16">
             SEND US YOUR COMMENTS
           </h2>
-
           <div className="grid md:grid-cols-3 gap-14">
             <form className="md:col-span-2 space-y-8">
               <div>
