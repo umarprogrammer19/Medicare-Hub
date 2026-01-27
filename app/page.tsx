eb9b2e11a36c5b0b8ba010cf36ec908c0ff04add
umarprogrammer19 <umarprogrammer19666@gmail.com>
2026-01-27 23:38:13 +0500

    Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,51 @@
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import Section1 from './components/Section1'
-import Section2 from './components/Section2'
-import Section3 from './components/Section3'
-import Section4 from './components/Section4'
-import Section5 from './components/Section5'
-import Section6 from './components/Section6'
-import Section7 from './components/Section7'
-import Section8 from './components/Section8'
-import Section9 from './components/Section9'
-import Section10 from './components/Section10'
-import Section11 from './components/Section11'
-import Section12 from './components/Section12'
-import Section14 from './components/Section14'
-import Section15 from './components/Section15'
-import Section16 from './components/Section16'
-import Section17 from './components/Section17'
-import Section18 from './components/Section18'
-import Section19 from './components/Section19'
-import Section20 from './components/Section20'
-import Section21 from './components/Section21'
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Section1 from './components/Section1';
+import Section2 from './components/Section2';
+import Section3 from './components/Section3';
+import Section4 from './components/Section4';
+import Section5 from './components/Section5';
+import Section6 from './components/Section6';
+import Section7 from './components/Section7';
+import Section8 from './components/Section8';
+import Section9 from './components/Section9';
+import Section10 from './components/Section10';
+import Section11 from './components/Section11';
+import Section12 from './components/Section12';
+import Section14 from './components/Section14';
+import Section15 from './components/Section15';
+import Section16 from './components/Section16';
+import Section17 from './components/Section17';
+import Section18 from './components/Section18';
+import Section19 from './components/Section19';
+import Section20 from './components/Section20';
+import Section21 from './components/Section21';
+
 export default function page() {
   return (
     <div>
-    <Navbar/>
-    <Home/>
-    <Section1/>
-    <Section2/>
-    <Section3/>
-    <Section4/>
-    <Section5/>
-    <Section6/>
-    <Section7/>
-    <Section8/>
-    <Section9/>
-    <Section10/>
-    <Section11/>
-    <Section12/>
-    <Section14/>
-    <Section15/>
-    <Section16/>
-    <Section17/>
-    <Section18/>
-    <Section19/>
-    <Section20/>
-    <Section21/>
+      <Navbar />
+      <Home />
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section5 />
+      <Section6 />
+      <Section7 />
+      <Section8 />
+      <Section9 />
+      <Section10 />
+      <Section11 />
+      <Section12 />
+      <Section14 />
+      <Section15 />
+      <Section16 />
+      <Section17 />
+      <Section18 />
+      <Section19 />
+      <Section20 />
+      <Section21 />
     </div>
   );
 }
