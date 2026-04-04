207a67b8772d9329b5ded7d9269f9e6878021245
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-04-04 23:04:58 +0500

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.vvariable} antialiased`}
       >
         {children}
       </body>
