3ee90557832cd95e9c2ca4a55a381aa15d08a6c8
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-06-13 23:46:06 +0500

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Medicare Hub",
+  title: "Medicare Hub Website",
   description: "Medical Landing Page",
 };
 
